6dcea415e395a9f9288ee4356d70490a4b74ec87
ECS\MikeYang <exia5693>
2018-05-11 10:17:13 +0800

    初版

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace ECS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mike去泡咖啡");
+            Console.WriteLine("站起來");
+            Console.WriteLine("走路");
+            Console.WriteLine("走路");
+            Console.WriteLine("轉彎");
+            Console.WriteLine("走路");
+            Console.WriteLine("走路");
+            Console.WriteLine("走路");
+            Console.WriteLine("轉彎");
+            Console.WriteLine("走路");
+            Console.WriteLine("轉彎");
+            Console.WriteLine("走路");
+            Console.WriteLine("磨咖啡");
+            Console.WriteLine("泡咖啡");
+            Thread.Sleep(5000);
+            Console.WriteLine("倒咖啡");
+            Console.WriteLine("走路");
+            Console.WriteLine("轉彎");
+            Console.WriteLine("走路");
+            Console.WriteLine("轉彎");
+            Console.WriteLine("走路");
+            Console.WriteLine("走路");
+            Console.WriteLine("走路");
+            Console.WriteLine("轉彎");
+            Console.WriteLine("走路");
+            Console.WriteLine("走路");
+            Console.WriteLine("坐下");
+        }
+    }
+}
